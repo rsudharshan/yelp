838727f92f88461ed89a716df67b0e30a7d9c7ee
sudharshan <srajendhiran@cognitivescale.com>
2019-06-14 11:14:05 +0530

    Recommender cfmodel added

--- recommendation/train.py
+++ recommendation/train.py
@@ -27,27 +27,7 @@ K_FACTORS = 100
 TEST_USER = 200 
 
 
-import numpy as np
-from keras.layers import Embedding, Reshape, Merge
-from keras.models import Sequential
-
-class CFModel(Sequential):
-    def __init__(self, n_users, m_items, k_factors, **kwargs):
-        P = Sequential()
-        P.add(Embedding(n_users, k_factors, input_length=1))
-        P.add(Reshape((k_factors,)))
-        Q = Sequential()
-        Q.add(Embedding(m_items, k_factors, input_length=1))
-        Q.add(Reshape((k_factors,)))
-
-        super(CFModel, self).__init__(**kwargs)
-        
-        # The Merge layer takes the dot product of user and movie latent factor vectors to return the corresponding rating.
-        self.add(Merge([P, Q], mode='dot', dot_axes=1))
-
-    # The rate function to predict user's rating of unrated items
-    def rate(self, user_id, item_id):
-        return self.predict([np.array([user_id]), np.array([item_id])])[0][0]
+from cfmodel import CFModel
 
 
 model = CFModel(N_USERS+1, N_RESTAURANTS+1, K_FACTORS)
